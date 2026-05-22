e8485b02a53b83155a1c542cc83e3087d4588562
MiltonTolaba <Milton.Tolaba@alu.frt.utn.edu.ar>
2026-05-22 15:25:41 -0300

    Completo CasoList

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -8,4 +8,27 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<string> alumnos = new List<string>();
+    public void AgregarAlumno(string alumno)
+    {
+        alumnos.Add(alumno);
+    }
+    public List<string> RetornarAlumnos()
+    {
+        return alumnos;
+    }
+    public bool BuscarAlumno(string nombre)
+    {
+        return alumnos.Contains(nombre);
+    }
+    public void EliminarAlumno(string alumno)
+    {
+        alumnos.Remove(alumno);
+    }
+    public void EliminarAlumnoEnPosicion(int posicion)
+    {
+        alumnos.RemoveAt(posicion);
+    }
 }
+
+
